2ae886fb15731c4e7e7e547cdd88e41337074673
parrt <parrt@cs.usfca.edu>
2016-09-01 14:34:42 -0700

    improved my guided random to like 38% by forcing sigma=1 everywhere.

--- src/nn/Main.java
+++ src/nn/Main.java
@@ -33,8 +33,8 @@ public class Main {
 			onehots[i] = new double[OUTPUT_LAYER_LEN];
 			onehots[i][labels[i]] = 1.0;
 		}
-		bareBonesParticleSwarmOptimizer(training);
-//		slightlyGuidedRandomSearchOptimizer(training);
+//		bareBonesParticleSwarmOptimizer(training);
+		slightlyGuidedRandomSearchOptimizer(training);
 	}
 
 	public static void bareBonesParticleSwarmOptimizer(List<Image> training) {
@@ -130,7 +130,7 @@ public class Main {
 				Network delta = genBest.subtract(globalBest);
 //				mu = globalBest.add(delta.scale(learningRate));
 				mu = genBest;
-				sigma = delta.abs();
+//				sigma = delta.abs();
 				globalBest = genBest;
 				globalLowestCost = genLowestCost;
 			}
